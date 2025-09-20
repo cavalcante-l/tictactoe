c50853c04c5fcabbfd1aa83169183c24712ca8e1
Lucas Cavalcante <l.cavalcante@aluno.ifsp.edu.br>
2025-09-19 21:52:46 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './app.css'
+import Board from './components/Board'
+import Titulo from './components/Titulo'
+
+function App() {
+
+  return (
+    <>
+      <Titulo/>
+      <Board />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Titulo.jsx
@@ -0,0 +1,6 @@
+export default function Titulo(){
+
+    return(
+        <h1>JOGO DA VELHA</h1>
+    )
+}
